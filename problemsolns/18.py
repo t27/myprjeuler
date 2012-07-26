53c7adc456ab8e8caa01bcf58416d67640a0140f
t27 <t27@email.com>
2012-07-26 14:19:32 +0530

    updating codes

---
+++ problemsolns/18.py
@@ -0,0 +1,22 @@
+
+
+
+mainlist=[]
+
+f=open("18triangle.txt")
+fstr=f.read()
+strlist=fstr.split('\n')
+for line in strlist:
+  mainlist.append(line.split(' '))
+mainlist.pop()
+n=len(mainlist)
+print mainlist
+for i in range(n-1,-1,-1):
+  for j in range(0,i):
+	print str(i)+"   "+str(j)
+	mainlist[i-1][j]=str(max( int(mainlist[i][j])+int(mainlist[i-1][j]) , int(mainlist[i][j+1])+int(mainlist[i-1][j]) ))
+
+for smlist in mainlist:
+  print smlist
+  
+  
---
+++ problemsolns/48.py
@@ -0,0 +1,7 @@
+
+sum1=0
+for i in range(1,1001):
+  sum1+=i**i
+
+print sum1
+print sum1%10000000000
